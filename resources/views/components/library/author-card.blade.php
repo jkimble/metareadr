f1a8dd09cd79e16eedfcb4a25d256f1e146f8654
Justin <jkimble.dev@gmail.com>
2025-06-22 23:51:42 -0400

    style author card

--- resources/views/components/library/author-card.blade.php
+++ resources/views/components/library/author-card.blade.php
@@ -1,6 +1,7 @@
 @props(['author'])
 
-<article class="flex-[0_1_32%] mb-4 p-4 bg-gray-800 text-white rounded">
+<article
+    class="flex-[0_1_32%] mb-4 p-4 bg-gray-800 border border-gray-800 text-white rounded hover:border-teal-500 transition">
     <h3 class="text-xl font-bold">{{ $author['name'] }}</h3>
     @if(!empty($author['top_work'] ))
         <p class="text-sm font-bold">
