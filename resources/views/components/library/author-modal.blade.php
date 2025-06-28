3b68e5a817d4884517f1dee73353c142cee095dd
Justin <jkimble.dev@gmail.com>
2025-06-28 19:25:37 -0400

    improve author modal layout, add down icon for scroll indicator

--- resources/views/components/library/author-modal.blade.php
+++ resources/views/components/library/author-modal.blade.php
@@ -11,13 +11,13 @@
          x-transition:leave-start="opacity-100"
          x-transition:leave-end="opacity-0"
          style="display: none;">
-        <div class="bg-gray-800 p-6 rounded-lg shadow-xl max-w-md w-full">
+        <div class="bg-gray-800 p-6 rounded-lg shadow-xl max-w-md w-full text-white">
             <div class="flex flex-row flex-nowrap items-start justify-between">
                 <div class="flex flex-col gap-2">
-                    <h3>{{ $authorInfo['name'] ?? 'huh' }} {{ !empty($authorInfo['title']) ? $authorInfo['title'] : '' }}</h3>
-                    <p>
+                    <h3 class="text-xl font-bold">{{ $authorInfo['name'] ?? '' }} {{ !empty($authorInfo['title']) ? $authorInfo['title'] : '' }}</h3>
+                    <p class="text-sm italic">
                         {{ $authorInfo['birth_date'] ?? '' }}
-                        {{--                        {{ !empty($authorInfo['death_date'] ? ' - ' . $authorInfo['death_date'] : '') }}--}}
+                        {{ ($authorInfo['death_date'] ?? '') ? ' - ' . $authorInfo['death_date'] : '' }}
                     </p>
                 </div>
                 <button @click="showModal = false" class="text-gray-400 hover:text-white hover:cursor-pointer">
@@ -26,9 +26,16 @@
             </div>
             <div class="flex flex-col gap-2 text-white mt-4">
                 @if(!empty($authorInfo['bio']))
-                    <p>
+                    <h4 class="font-bold mb-2">About {{ $authorInfo['name'] }}:</h4>
+                    <p class="max-h-36 overflow-y-scroll">
                         {{ $authorInfo['bio']['value'] }}
                     </p>
+                    <span class="text-xs text-center italic">
+                        Scroll for more
+                        <span class="animate-bounce inline-block">
+                            <x-icons.down/>
+                        </span>
+                    </span>
                 @endif
                 <div class="flex flex-row flex-nowrap items-center justify-between gap-2 mt-4">
                     <x-content.button styling="secondary" class="btn-xs" @click="showModal = false">
